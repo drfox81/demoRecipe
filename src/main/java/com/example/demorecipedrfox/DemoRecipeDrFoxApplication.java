1909320a13026887a8bbe52ad19063e3a31a1784
Drfox81 <Drfox11181@mail.ru>
2023-01-16 11:30:02 +0300

    1

---
+++ src/main/java/com/example/demorecipedrfox/DemoRecipeDrFoxApplication.java
@@ -0,0 +1,13 @@
+package com.example.demorecipedrfox;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoRecipeDrFoxApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoRecipeDrFoxApplication.class, args);
+    }
+
+}
